1379ab123fca7b826554228f5f368a771ccb8e1b
bahati7 <bahatiphilmon8@gmail.com>
2022-05-13 12:00:50 +0200

    main update

--- Js/script.js
+++ Js/script.js
@@ -1,22 +1,13 @@
-"use strict";
+const showMenu=(toggleId,navId)=>{
+    const toggle=document.getElementById(toggleId),
+    nav=document.getElementById(navId);
 
-const humburger=document.getElementById("humburger"); 
-const header=document.getElementById("header");
-const navLinks=document.querySelectorAll(".nav-link");
+    if(toggle && nav){
+        toggle.addEventListener('click',()=>{
+            nav.classList.toggle('show')
+            toggle.classList.toggle('humburger')
+        })
+    }
+}
 
-humburger.addEventListener("click",()=>{
-    header.classList.toggle("active");
-});
-navLinks.forEach(function(nav){
-    nav.addEventListener("click",function(){
-        //console.log(this);
-        this.classList.toggle("open");
-
-        if(nav.classList.contains("open")){
-            navLinks.forEach(function(nav){
-                nav.classList.remove("open");
-            });
-            nav.classList.toggle("open");
-        }
-    });
-});
+showMenu('header_toggle','nav-menu')
